98c12737df480381c917041c4517a874ae5fc3a8
Ferperemero86 <fernandoperezmerono@gmail.com>
2023-09-17 08:36:58 +0100

    Create Main layout

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="fixed z-[-1] bg-[#f2f2f2] w-full h-[100vh]">
+    <NuxtPage />
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script setup>
+import MainLayout from "@/layouts/MainLayout.vue";
+</script>
+
+<template>
+  <MainLayout>
+    <div id="IndexPage" class="mt-4 max-w-[1200px] mx-auto px-2">IndexPage</div>
+  </MainLayout>
+</template>
