250460914fad75188331206a188f8b8a4a13cd72
Ferperemero86 <fernandoperezmerono@gmail.com>
2023-09-17 08:36:16 +0100

    Add nuxt, eslint/prettier config

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ["plugin:vue/vue3-recommended", "prettier"],
+
+  rules: {
+    "vue/comment-directive": "off",
+    "vue/multi-word-component-names": "off",
+    "vue/no-lone-template": "off",
+  },
+};
